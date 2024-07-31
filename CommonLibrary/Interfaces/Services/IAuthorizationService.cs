637f9e0987169b00bb98d4688c6a641caf1ec6b1
Mikhuil-Kop <44004552+Mikhuil-Kop@users.noreply.github.com>
2024-07-31 18:09:16 +0300

    Изменения IAuthorizationService

--- CommonLibrary/Interfaces/Services/IAuthorizationService.cs
+++ CommonLibrary/Interfaces/Services/IAuthorizationService.cs
@@ -2,12 +2,6 @@
 {
     public interface IAuthorizationService
     {
-        public Task RegisterAsync(string login, string password);
-        public Task RegisterConfirmationAsync();
-
-        public Task<string> LoginAsync(string login, string password);
-        public Task LogOutAsync(string token);
-
         public Task<int> GetAuthorisedUserIDAsync(string token);
     }
 }
