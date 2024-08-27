de751ca508245cfca0e7be66b8840e915af61007
Mikhuil-Kop <m.kopotov@gmail.ru>
2024-08-27 20:13:38 +0300

    Модель сообщений для Notification

---
+++ ModelLibrary/Messages/NotificationMessage.cs
@@ -0,0 +1,11 @@
+﻿using ModelLibrary.Enums;
+
+namespace ModelLibrary.Messages
+{
+    public class NotificationMessage
+    {
+        public int UserID { get; set; }
+        public NotificationType NotificationsType { get; set; }
+        public List<NotificationMessageParam>? Params { get; set; }
+    }
+}
---
+++ ModelLibrary/Messages/NotificationMessageParam.cs
@@ -0,0 +1,8 @@
+﻿namespace ModelLibrary.Messages
+{
+    public class NotificationMessageParam
+    {
+        public required string Name { get; set; }
+        public required string Value { get; set; }
+    }
+}
